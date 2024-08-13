e2a118a6685ed8b9c469e02c09ee1d0bb1f29440
luisinhu <luishenriquesb30@gmail.com>
2024-08-13 15:01:20 -0400

    new file:   resources/views/Home.blade.php 	new file:   resources/views/ListarPedidos.blade.php 	new file:   resources/views/ListarProdutos.blade.php

---
+++ resources/views/ListarPedidos.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello PHP and World!</h1>
+    <div>
+        <nav>
+            <ul>
+                <a href="{{ route('/home') }}"><li>HOME</li></a>
+                <a href="{{ route('/listar-pedidos') }}"><li>PEDIDOSt</li></a>
+                <a href="{{ route('/listar-produtos') }}"><li>PRODUTOS</li></a>
+            </ul>
+        </nav>
+    </div>
+    <h1>Essa é a minha página (Listar Pedidos)</h1>
+</body>
+</html>
